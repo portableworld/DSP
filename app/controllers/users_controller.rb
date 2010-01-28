7635631419a9988adf852c3e1aae082fbe8f9236
portableworld <bookedge@gmail.com>
2010-01-27 21:50:33 -0700

    Fixed some errors in content_add. They pertained to my usage of 'empty?'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,32 +3,46 @@ layout "show_content"
 
   before_filter :get_user_id
   def index
+    # Get the date of the current show that the User is working on
     @show_date = ShowdateLink.find_by_user_id(@user)
+    # If it is the User's first time, they won't have a show_date
+    # We need to create one for the current day.
     if @show_date == nil
       s = ShowdateLink.new
       s.user_id = current_user.id
       s.showdate = Date.today
       s.save!
+      # Now that they have a show_date, we can grab it and use it.
       @show_date = ShowdateLink.find_by_user_id(@user)
     end
     
+    # This should grab both 'content' and 'link'
     @contents = Compilation.find_all_by_user_id_and_show_date(@user, @show_date.showdate)
+    # The order is not set when a new Compilation is added to the database. 
+    # This gives it an order if it doesn't have one
+    # This will end up giving several items the same order but
+    # this does not cause confliction
     for content in @contents do
       if content.order == nil
         content.order = @contents.length
       end
     end
     
+    # Now that they all have orders, we order them. 
     @contents = @contents.sort_by { |content| content[:order] }
 
+    # Now we create an array and populate it with our content
+    # Is this needed? 
     @show = Array.new
     for content in @contents do
-      @picked = Content.find(content.content_id)
+      @picked = Content.find(content.content_id) if content.content_id
+      @picked = Link.find(content.link_id) if content.link_id
       @show << @picked
     end
     
   end
   
+  # Don't remember what this method is for.
   def contents
     @content = @user.contents
   end
@@ -59,45 +73,45 @@ layout "show_content"
   end
   
   def content_add
-	# Should first check params to see if incoming content is 'Content' or 'Link'	
-	unless params[:content].empty? then
-		@content = Content.find(params[:content])
-		@link = ""
-	else
-		@link = Link.find(params[:link])
-		@content = ""
-	end
-	
-	# Get date of the show the content is for
+  # Should first check params to see if incoming content is 'Content' or 'Link' 
+  unless params[:content] == nil then
+    @content = Content.find(params[:content])
+    @link = ""
+  else
+    @link = Link.find(params[:link])
+    @content = ""
+  end
+  
+  # Get date of the show the content is for
     @show_date = ShowdateLink.find_by_user_id(@user)
-	
-	# Start up new Compilation and then populate it
+  
+  # Start up new Compilation and then populate it
     c = Compilation.new  
     c.user_id = current_user.id
-    c.content_id = @content.id unless @content.empty? # Only populates c if there's anything in @content
-	c.link_id = @link.id unless @link.empty? # Same as above
+    c.content_id = @content.id unless @content.to_s.empty? # Only populates c if there's anything in @content
+  c.link_id = @link.id unless @link.to_s.empty? # Same as above
     c.show_date = @show_date.showdate
     c.save!
-	
-	# Bounce back nothing. 
+  
+  # Bounce back nothing. 
     render :nothing => true
   end
 
   def content_delete
-	# Should first check params to see if incoming content is 'Content' or 'Link'	
-	unless params[:content].empty? then
-		@content = Compilation.find_all_by_content_id_and_user_id(params[:content], current_user.id)
-		@content[0].destroy
-	else
-		@link = Compilation.find_all_by_link_id_and_user_id(params[:link], current_user.id)
-		@link[0].destroy
-	end
-	
-	# Bounce back nothing
+  # Should first check params to see if incoming content is 'Content' or 'Link' 
+  unless params[:content].empty? then
+    @content = Compilation.find_all_by_content_id_and_user_id(params[:content], current_user.id)
+    @content[0].destroy
+  else
+    @link = Compilation.find_all_by_link_id_and_user_id(params[:link], current_user.id)
+    @link[0].destroy
+  end
+  
+  # Bounce back nothing
     render :nothing => true
   end
 
-	# This def is marked for deletion
+  # This def is marked for deletion
   def link_add
     @link = Link.find(params[:link])
     @show_date = ShowdateLink.find_by_user_id(@user)
@@ -111,7 +125,7 @@ layout "show_content"
     render :nothing => true
   end
 
-	# This def is marked for deletion
+  # This def is marked for deletion
   def link_delete
     @linkalation = Linkalation.find_by_link_id_and_user_id(params[:link], current_user.id)
 
